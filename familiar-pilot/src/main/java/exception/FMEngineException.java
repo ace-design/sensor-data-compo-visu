7cd7e4a43a4c06f8553693e4294d499cd7a60a87
Ivan L <logre@polytech.unice.fr>
2014-06-24 09:28:23 +0200

    Initiate the API for piloting Familiar from Java. Import existing code from Simon Urli.
    Note: Familiar doesn't expose a pom.xml, so the pilot pom has to bring familiar dependencies by hand...

---
+++ familiar-pilot/src/main/java/exception/FMEngineException.java
@@ -0,0 +1,11 @@
+package exception;
+
+/**
+ * Created by ivan on 23/06/2014.
+ */
+public class FMEngineException extends Exception  {
+
+    public FMEngineException(String string) {
+        super(string);
+    }
+}
