d3b25a99b03d6c8e1ea509ec793c1129e6c73d33
Ivan L <logre@polytech.unice.fr>
2014-06-27 16:30:11 +0200

    Add a generic example to demonstrate both concerns

--- visu-compo-meta-model/src/main/java/exampleslibrary/GenericTargeting.java
+++ visu-compo-meta-model/src/main/java/exampleslibrary/GenericTargeting.java
@@ -8,6 +8,7 @@ import utils.FileOperation;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 import static utils.CodeGeneration.codeGeneration;
 
@@ -28,24 +29,49 @@ public class GenericTargeting {
 
         //Design the model of the wanted dashboard
         Data data = new Data("http://users.polytech.unice.fr/~logre/resources/temp2.senml");
-        Concern concern = Concern.Discrete;
-        Visualization visu = new Visualization(data, concern);
+        Visualization visu = new Visualization(data);
+
+        System.out.println("What Concern do you want your visualization to fulfill ?");
+
+        Boolean validEntry = false;
+        String entry ="";
+        do {
+            System.out.println("Available Concerns are :");
+            for (Concern c : Concern.values())
+                System.out.println("     - "+c.toString());
+            Scanner scan = new Scanner(System.in);
+
+            entry = scan.nextLine();
+            for (Concern c : Concern.values()){
+                if(c.toString().equals(entry)) {
+                    validEntry = true;
+                    break;
+                }
+            }
+        }while(!validEntry);
+        visu.addConcern(Concern.valueOf(entry));
+
+        System.out.println("Concern "+entry+" added to your visualization.");
+        System.out.println("Let see what we got for you...");
+
 
         //Use feature model to find a suitable generable widget
         FMExposer exposer = new FMExposer();
         String config = exposer.newConfig();
-        if(exposer.reduceByConcern(visu.getConcern().toString(),config)==1) // there is only one configuration available after this reduction
+        if(exposer.reduceByConcern(visu.getConcern().toString(),config)==1){ // there is only one configuration available after this reduction
+            System.out.println("There is only one widget suitable for your visualization requirements. Let's generate it.");
             visu.setWidgetName(exposer.getWidgetName(config).replace(" ",""));
 
-        //Generation of the HTML code from the model
-        String code = codeGeneration(visu);
+            //Generation of the HTML code from the model
+            String code = codeGeneration(visu);
 
-        //Creation of the /product folder if it doesn't exist already
-        File f = new File(Paths.get("").toAbsolutePath().toString()+"/visu-compo-meta-model/products/");
-        if((!f.exists())||(!f.isDirectory())){f.mkdirs();}
-
-        //store the resulting visualization in a file named after the used concern
-        FileOperation.fillFileFromObject(code, Paths.get("").toAbsolutePath().toString() + "/visu-compo-meta-model/products/" + concern.toString() + ".html");
+            //Creation of the /product folder if it doesn't exist already
+            File f = new File(Paths.get("").toAbsolutePath().toString()+"/visu-compo-meta-model/products/");
+            if((!f.exists())||(!f.isDirectory())){f.mkdirs();}
+            //store the resulting visualization in a file named after the used concern
+            FileOperation.fillFileFromObject(code, Paths.get("").toAbsolutePath().toString() + "/visu-compo-meta-model/products/" + entry.toString() + ".html");
+            System.out.println("OK ! Go find your html visualization in the product folder.");
+        }
     }
 
 
